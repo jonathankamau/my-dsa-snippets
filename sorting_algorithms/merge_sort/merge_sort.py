e2f316e890563bd347d2bc1e72377a09c88b3311
Jonathan Kamau <jonathan.kamau@andela.com>
2018-10-03 07:46:32 +0300

    Update merge_sort.py

--- sorting_algorithms/merge_sort/merge_sort.py
+++ sorting_algorithms/merge_sort/merge_sort.py
@@ -1,9 +1,1 @@
-def merge_sort(unsorted_list,left_index,right_index): 
-    if left_index < right_index: 
 
-        middle = (left_index+(right_index-1))/2
-  
-
-        mergeSort(arr, l, m) 
-        mergeSort(arr, m+1, r) 
-        merge(arr, l, m, r) 
