555c54ceb383d812e4221f6189156c6b3cb4754f
Jonathan Kamau <jonathan.kamau@andela.com>
2019-08-19 20:56:09 +0300

    encapsulate the node_to traversal in a try-catch

--- assessment_feedback_implementation/breadth_first_search.py
+++ assessment_feedback_implementation/breadth_first_search.py
@@ -1,33 +1,49 @@
 '''
 Breadth First Search Implementation
 '''
-from graphs import graph
 import pprint
+from graph import graph
 
 pp = pprint.PrettyPrinter(indent=2)
 
 
-def bfs(self, start_node_num, search_term):
-    """Breadth First Search with queue data structure."""
-    start_node = graph.find_node(start_node_num)
+class BreadthFirstSearch:
+    """Breadth First Search class initialization."""
 
-    queue = [start_node]
-    message = 'Search Item not found!'
+    def __init__(self):
+        self.graph = graph
 
-    while queue:
-        current_node = queue.pop(0)
-        current_node.visited = True
-        if self.node_names[current_node.value] == search_term:
-            message = 'Found the Search Item! ' + \
-                graph.node_names[current_node.value]
-        for edge in current_node.edges:
-            if not edge.node_to.visited:
-                edge.node_to.visited = True
-                queue.append(edge.node_to)
+    def bfs_search(self, start_node_num, search_term):
+        """Breadth First Search with queue data structure."""
+        start_node = self.graph.find_node(start_node_num)
 
-        return message
+        queue = [start_node]
+        message = 'Search Item not found!'
+
+        while queue:
+            current_node = queue.pop(0)
+            current_node.visited = True
+            if self.graph.node_names[current_node.value] == search_term:
+                message = 'Found the Search Item! ' + \
+                    self.graph.node_names[current_node.value]
+            else:
+                try:
+                    for edge in current_node.edges:
+                        if (self.graph.node_names[
+                                edge.node_to.value] == search_term):
+                            message = 'Found the Search Item! ' + \
+                                self.graph.node_names[edge.node_to.value]
+                            break
+                        elif not edge.node_to.visited:
+                            edge.node_to.visited = True
+                            queue.append(edge.node_to)
+                except Exception as error:
+                    return error
+
+            return message
 
 
-print "Depth First Search with search item"
+bfs = BreadthFirstSearch()
+print "Breadth First Search with search item"
 graph._clear_visited()
-
+pp.pprint(bfs.bfs_search(2, 'Mombasa'))
--- assessment_feedback_implementation/depth_first_search.py
+++ assessment_feedback_implementation/depth_first_search.py
@@ -1,34 +1,47 @@
 '''
 Depth First Search Implementation
 '''
-from graphs import graph
 import pprint
+from graph import graph
 
 pp = pprint.PrettyPrinter(indent=2)
 
 
-def dfs_search(self, start_node_num, search_term):
-    """Depth First Search Implementation."""
-    start_node = graph.find_node(start_node_num)
-    message = 'Search Item not found!'
+class DepthFirstSearch:
+    """Depth First Search class initialization."""
 
-    if graph.node_names[start_node.value] == search_term:
-        start_node.visited = True
-        message = 'Found the Search Item! ' + \
-            graph.node_names[start_node.value]
-    else:
-        for edge in start_node.edges:
-            if graph.node_names[edge.node_to.value] == search_term:
-                message = 'Found the Search Item! ' + \
-                    graph.node_names[edge.node_to.value]
-                break
-            elif not edge.node_to.visited:
-                edge.node_to.visited = True
-                graph.dfs_search(edge.node_to.value, search_term)
+    def __init__(self):
+        self.graph = graph
 
-    return message
+    def dfs_search(self, start_node_num, search_term):
+        """Depth First Search Implementation."""
+        start_node = self.graph.find_node(start_node_num)
+        message = 'Search Item not found!'
+
+        if self.graph.node_names[start_node.value] == search_term:
+            start_node.visited = True
+            message = 'Found the Search Item! ' + \
+                self.graph.node_names[start_node.value]
+        else:
+            try:
+                for edge in start_node.edges:
+                    if (self.graph.node_names[
+                            edge.node_to.value] == search_term):
+                        message = 'Found the Search Item! ' + \
+                            self.graph.node_names[edge.node_to.value]
+                        break
+                    elif not edge.node_to.visited:
+                        edge.node_to.visited = True
+                        self.dfs_search(edge.node_to.value, search_term)
+                    else:
+                        message = 'Problem with search code!'
+            except Exception as error:
+                return error
+
+        return message
 
 
+dfs = DepthFirstSearch()
 print "Depth First Search with search item"
 graph._clear_visited()
-pp.pprint(graph.dfs_search(2, 'Kigali'))
+pp.pprint(dfs.dfs_search(2, 'Kigali'))
